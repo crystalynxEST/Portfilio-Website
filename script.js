ac79a66f02751d91dcde28e1d58f593f57c3b4ad
Kristi Jaerve <valgeroos@gmail.com>
2023-02-08 09:17:07 +0000

    Style introduction, add slider

---
+++ script.js
@@ -0,0 +1,29 @@
+$(".parent_slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    dotsClass: "slickkdots",
+    arrows:false,
+});
+
+
+/*
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+*/
+
